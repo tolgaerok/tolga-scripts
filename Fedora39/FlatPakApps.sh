756876689194146c16d40a13b330314452e53093
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-11-19 14:15:00 +0800

    Update FlatPakApps.sh

--- Fedora39/FlatPakApps.sh
+++ Fedora39/FlatPakApps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Tolga Erok
+# 19/11/23
 
 # Check if Flatpak is installed
 if ! command -v flatpak &> /dev/null; then
@@ -6,16 +8,20 @@ if ! command -v flatpak &> /dev/null; then
     exit 1
 fi
 
-# Install Caprine
-flatpak install flathub com.github.desdelinux.caprine
+# Add Flathub repository if not already added
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install Shotwell
-flatpak install flathub org.gnome.Shotwell
+# Define an array of Flatpak application IDs
+flatpak_apps=(
+    "com.sindresorhus.Caprine"
+    "org.gnome.Shotwell"
+    "com.transmissionbt.Transmission"
+    "im.pidgin.Pidgin"
+)
 
-# Install Transmission
-flatpak install flathub com.transmissionbt.Transmission
-
-# Install Pidgin
-flatpak install flathub im.pidgin.Pidgin
+# Install applications
+for app in "${flatpak_apps[@]}"; do
+    flatpak install flathub "$app"
+done
 
 echo "Installation completed. You can now run the installed applications."
