c6c25dfa6e812d0e413bfeb2a082d543722b86b9
Tolga Erok <kingtolga@gmail.com>
2023-11-24 05:10:43 +0000

    -

--- Fedora39/TolgasFedoraUpdaterApp.py
+++ Fedora39/TolgasFedoraUpdaterApp.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 import subprocess
+import pexpect
 
 class TolgasFedoraUpdaterApp:
     def __init__(self, root):
@@ -26,7 +28,7 @@ class TolgasFedoraUpdaterApp:
         self.btn_configure_dnf.pack(pady=10)
 
         # Terminal-like output
-        self.output_text = tk.Text(root, height=10, width=80, wrap="word")
+        self.output_text = ScrolledText(root, height=10, width=80, wrap="word")
         self.output_text.pack(pady=10)
 
     def update_system(self):
@@ -39,14 +41,18 @@ class TolgasFedoraUpdaterApp:
         self.output_text.config(state="normal")
         self.output_text.delete(1.0, tk.END)
 
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True)
+        process = pexpect.spawn(' '.join(command), timeout=None)
 
         def update_output():
-            line = process.stdout.readline()
-            if line:
-                self.output_text.insert(tk.END, line)
-                self.output_text.see(tk.END)
-                self.root.after(100, update_output)  # Schedule the next update in 100 milliseconds
+            try:
+                while True:
+                    line = process.readline()
+                    if not line:
+                        break
+                    self.output_text.insert(tk.END, line)
+                    self.output_text.see(tk.END)
+            except pexpect.EOF:
+                pass
 
         update_output()
 
