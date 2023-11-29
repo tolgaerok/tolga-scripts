21aa96ab34c879fd134933a94eb587791cd53897
Tolga Erok <kingtolga@gmail.com>
2023-11-29 12:20:23 +0800

    vs

--- Fedora39/TolgaFedora39.sh
+++ Fedora39/TolgaFedora39.sh
@@ -785,7 +785,7 @@ fix_grub() {
 }
 
 # Remove KDE Junk
-remove_kde_crap() {
+remove_kde_crap  () {
 
     # List of KDE applications to check
     apps=("akregator" "ksysguard" "dnfdragora" "kfind" "kmag" "kmail" "kcolorchooser" "kmouth" "korganizer" "kmousetool" "kruler" "kaddressbook" "kcharselect" "konversation" "elisa-player" "kmahjongg" "kpat" "kmines" "dragonplayer" "kamoso" "kolourpaint" "krdc" "krfb")
