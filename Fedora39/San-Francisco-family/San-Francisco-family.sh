2251c254b80610ccb6c52c5602b8e7208049c0d6
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-12 12:01:59 +0800

    Update San-Francisco-family.sh

--- Fedora39/San-Francisco-family/San-Francisco-family.sh
+++ Fedora39/San-Francisco-family/San-Francisco-family.sh
@@ -4,7 +4,7 @@
 # My personal San-Francisco-family Font Downloader
 # 18/11/2023
 # Run from remote location:::...1112
-# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/tolga-scripts/main/Fedora39/TolgaFedora39.sh)"
+# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/tolga-scripts/main/Fedora39/San-Francisco-family/San-Francisco-family.sh)"
 clear
 
 # Check if the script is run as root
