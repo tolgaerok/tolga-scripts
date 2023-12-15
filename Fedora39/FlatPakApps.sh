9bc7813645a850b42410707368751d88155093e5
Tolga Erok <kingtolga@gmail.com>
2023-12-15 14:26:49 +0000

    fp

--- Fedora39/FlatPakApps.sh
+++ Fedora39/FlatPakApps.sh
@@ -18,6 +18,19 @@
 #   ░ ░       ░    ░ ░  ░ ░ ░ ░ ▒    ░░   ░   ░   ▒
 #   ░  ░      ░    ░ ░     ░              ░  ░   ░
 
+# Check if script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root. Please use sudo."
+    exit 0
+fi
+
+# Prompt user for confirmation
+read -p "This script will install Flatpak applications and make system modifications. Do you want to continue? (y/n): " choice
+if [[ ! "$choice" =~ ^[Yy]$ ]]; then
+    echo "Installation aborted."
+    exit 0
+fi
+
 # Check if Flatpak is installed
 if ! command -v flatpak &>/dev/null; then
     echo "Flatpak is not installed. Please install Flatpak and run the script again."
@@ -61,8 +74,11 @@ flatpak override --user --filesystem=xdg-data/applications com.usebottles.bottle
 # Allow Bottles to access Steam folder
 flatpak override --user --filesystem=home/.var/app/com.valvesoftware.Steam/data/Steam com.usebottles.bottles
 
-# Install Breeze-GTK flatpak theme
-flatpak install -y flathub org.gtk.Gtk3theme.Breeze
+# Check if Breeze-GTK theme is already installed
+if ! flatpak list | grep -q org.gtk.Gtk3theme.Breeze; then
+    # Install Breeze-GTK flatpak theme
+    flatpak install -y flathub org.gtk.Gtk3theme.Breeze
+fi
 
 # Install applications
 flatpak install -y flathub org.videolan.VLC
@@ -91,17 +107,38 @@ timeout 5 flatpak run org.mozilla.firefox
 # Set Firefox profile path
 # FIREFOX_PROFILE_PATH=$(realpath ${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox/*.default-release)
 # FIREFOX_PROFILE_PATH=$(realpath "${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox"/*.default-release)
-FIREFOX_PROFILE_PATH=$(find "${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox/" -maxdepth 1 -type d -name '*.default-release')
+# FIREFOX_PROFILE_PATH=$(find "${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox/" -maxdepth 1 -type d -name '*.default-release')
+FIREFOX_PROFILE_PATH=$(find "${HOME}/.mozilla/firefox" -name "*.default-release")
 
+# Check if the profile directory is found
 if [ -z "$FIREFOX_PROFILE_PATH" ]; then
     echo "Error: Firefox profile directory not found."
     exit 1
 fi
 
+# Create or update the user.js file in the Firefox profile directory
+cat <<EOL >"$FIREFOX_PROFILE_PATH/user.js"
+// Firefox Preferences
+user_pref("extensions.pocket.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("geo.enabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.search.defaultenginename", "Google");
+user_pref("webgl.disabled", False);
+EOL
+
+# Restart Firefox
+flatpak run org.mozilla.firefox
+
 # Disable D-Bus warnings in Firefox
 # echo 'pref("toolkit.startup.max_resumed_crashes", -1);' >>${FIREFOX_PROFILE_PATH}/user.js
 # echo 'pref("toolkit.startup.max_resumed_crashes", -1);' >>"${FIREFOX_PROFILE_PATH}/user.js"
-echo 'pref("toolkit.startup.max_resumed_crashes", -1);' >> "${FIREFOX_PROFILE_PATH}/user.js"
+echo 'pref("toolkit.startup.max_resumed_crashes", -1);' >>"${FIREFOX_PROFILE_PATH}/user.js"
 
 # Import extensions
 mkdir -p ${FIREFOX_PROFILE_PATH}/extensions
@@ -109,6 +146,45 @@ curl https://addons.mozilla.org/firefox/downloads/file/4003969/ublock_origin-lat
 curl https://addons.mozilla.org/firefox/downloads/file/4018008/bitwarden_password_manager-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/{446900e4-71c2-419f-a6a7-df9c091e268b}.xpi
 curl https://addons.mozilla.org/firefox/downloads/file/3998783/floccus-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/floccus@handmadeideas.org.xpi
 curl https://addons.mozilla.org/firefox/downloads/file/3932862/multi_account_containers-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/@testpilot-containers.xpi
+
+# uBlock Origin - An efficient ad-blocker for various web browsers
+curl https://addons.mozilla.org/firefox/downloads/file/420571/ublock_origin-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/ublock_origin@raymondhill.net.xpi
+
+# SponsorBlock - Skip sponsored segments on YouTube
+curl https://addons.mozilla.org/firefox/downloads/file/421708/sponsorblock_for_youtube_0.5.1_an+fx.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/sponsorblock_for_youtube@spicetools.xpi
+
+# Video Downloader PLUS - Download videos from popular websites
+curl https://addons.mozilla.org/firefox/downloads/file/427463/video_downloader_plus-1.4.1-an+fx.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/video_downloader_plus@andy.porter.xpi
+
+# Decentraleyes - Protects you against tracking through "free", centralized, content delivery
+curl https://addons.mozilla.org/firefox/downloads/file/503313/decentraleyes-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/decentraleyes@decentraleyes.org.xpi
+
+# Privacy Possum - A privacy protection extension that detects and blocks surveillance trackers
+curl https://addons.mozilla.org/firefox/downloads/file/556049/privacy_possum-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/privacy_possum@tohodo.com.xpi
+
+# Auto Tab Discard - Automatically discards inactive tabs to free up system resources
+curl https://addons.mozilla.org/firefox/downloads/file/573664/auto_tab_discard-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/auto_tab_discard@piro.sakura.ne.jp.xpi
+
+# Disconnect - Visualizes and blocks the invisible sites that track your search and browsing history
+curl https://addons.mozilla.org/firefox/downloads/file/442786/disconnect-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/disconnect@disconnect.me.xpi
+
+# Canvas Blocker - Provides additional privacy protection by preventing canvas fingerprinting
+curl https://addons.mozilla.org/firefox/downloads/file/432753/canvasblocker-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/canvasblocker@kkapsner.de.xpi
+
+# User Agent Switcher and Manager - Switch user agents and manage custom user agent strings
+curl https://addons.mozilla.org/firefox/downloads/file/683079/user_agent_switcher_and_manager-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/{2b10c1c8-a11f-4bad-fe9c-1c11e82cac42}.xpi
+
+# Don't Track me Google - Prevents Google from tracking your searches and personalizing search results
+curl https://addons.mozilla.org/firefox/downloads/file/557150/dont_track_me_google-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/dont_track_me_google@rob.xpi
+
+# YouTube NonStop - Prevents YouTube videos from buffering and stops autoplay
+curl https://addons.mozilla.org/firefox/downloads/file/416109/youtube_nonstop-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/youtube_nonstop@manizuca.xpi
+
+# Facebook Container - Isolates your Facebook identity into a separate container
+curl https://addons.mozilla.org/firefox/downloads/file/584616/facebook_container-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/facebook_container@mozilla.org.xpi
+
+# Dark Reader - Enables dark mode on websites to reduce eye strain
+curl https://addons.mozilla.org/firefox/downloads/file/436147/dark_reader-latest.xpi -o ${FIREFOX_PROFILE_PATH}/extensions/dark_reader@darkreader.org.xpi
 
 # KDE specific configurations
 tee -a ${FIREFOX_PROFILE_PATH}/user.js <<'EOF'
@@ -134,6 +210,7 @@ flatpak_apps=(
 for app in "${flatpak_apps[@]}"; do
     flatpak install flathub "$app"
 done
+
 echo -e "\e[1;32m[✔]\e[0m Checking updates for installed flatpak programs...\n"
 flatpak update -y
 sleep 1
