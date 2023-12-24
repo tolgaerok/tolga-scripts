50a29c2593e21d90cfc7746eec5f25a71ccd5adf
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-25 02:57:53 +0800

    Update nix-solus.sh

--- NIXOS-PKG-MANAGER/SOLUS/nix-solus.sh
+++ NIXOS-PKG-MANAGER/SOLUS/nix-solus.sh
@@ -5,22 +5,56 @@
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/tolga-scripts/main/NIXOS-PKG-MANAGER/SOLUS/nix-solus.sh)"
 
 clear
+# Assign a color variable based on the RANDOM number
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+CYAN='\e[1;36m'
+WHITE='\e[1;37m'
+ORANGE='\e[1;93m'
+NC='\e[0m'
+YELLOW='\e[1;33m'
+NC='\e[0m'
+
+# Template
+# display_message "[${GREEN}✔${NC}]
+# display_message "[${RED}✘${NC}]
+
+# Function to display messages
+display_message() {
+    clear
+    echo -e "\n                  Tolga's online Nix-Package setup\n"
+    echo -e "\e[34m|--------------------\e[33m Currently configuring:\e[34m-------------------|"
+    echo -e "|${YELLOW}==>${NC}  $1"
+    echo -e "\e[34m|--------------------------------------------------------------|\e[0m"
+    echo ""
+   
+}
 
 # Ask for the username
+display_message "[${GREEN}✔${NC}] User-name required"
+echo ""
 read -p "Enter the username: " username
 
 # setup nix pkgs on solus
 cd /home/$username
 
+display_message "[${GREEN}✔${NC}] Downloading nix pgk manager"
+
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 . /home/$username/.nix-profile/etc/profile.d/nix.sh
 nix --version
+
+display_message "[${GREEN}✔${NC}] Setting up profile PATHS"
 
 # Add the following lines to append export statements to .bashrc
 echo "export PATH=\"/home/$username/.nix-profile/bin:\$PATH\"" >> /home/$username/.bashrc
 echo ". /home/$username/.nix-profile/etc/profile.d/nix.sh" >> /home/$username/.bashrc
 
+sleep 2
 
+display_message "[${GREEN}✔${NC}] Setting up default.nix in $HOME"
 
 # Create a default.nix file in the home directory
 echo '{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell { buildInputs = [ pkgs.nixpkgs-fmt ]; }' > /home/$username/default.nix
@@ -28,6 +62,11 @@ echo '{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell { buildInputs = [ pkgs.nixpkg
 # Source the modified .bashrc to apply changes without restarting the shell
 source /home/$username/.bashrc
 
+sleep 2
+
 # Verify that the changes took effect
 nix --version
 nix-shell
+
+echo -e"\nDone\n"
+
