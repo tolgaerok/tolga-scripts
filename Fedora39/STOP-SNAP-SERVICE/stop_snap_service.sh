df4cfe4e5b106ae6c494cb341c6bb2351f72dd51
Tolga Erok <kingtolga@gmail.com>
2023-12-31 09:15:38 +0000

    ff

--- Fedora39/STOP-SNAP-SERVICE/stop_snap_service.sh
+++ Fedora39/STOP-SNAP-SERVICE/stop_snap_service.sh
@@ -4,7 +4,7 @@
 # 31/12/203
 
 # Run from remote location:
-# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/tolga-scripts/main/Fedora39/STOP-SNAP-SERVICE/stop_snap_service.sh"
+# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/tolga-scripts/main/Fedora39/STOP-SNAP-SERVICE/stop_snap_service.sh)"
 
 #  ¯\_(ツ)_/¯
 #    █████▒▓█████ ▓█████▄  ▒█████   ██▀███   ▄▄▄
