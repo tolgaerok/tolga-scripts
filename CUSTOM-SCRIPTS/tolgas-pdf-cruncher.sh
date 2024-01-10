328371e8204bc062fd168ed498753e482e610e93
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-01-10 12:52:46 +0800

    Update tolgas-pdf-cruncher.sh

--- CUSTOM-SCRIPTS/tolgas-pdf-cruncher.sh
+++ CUSTOM-SCRIPTS/tolgas-pdf-cruncher.sh
@@ -6,44 +6,47 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Check arguments is providedd
-if [ "$#" -ne 2 ]; then
-  echo "Usage: pdfcompress <input_file> <output_file>"
+if [ "$#" -lt 1 ]; then
+  echo "Usage: pdfcompress <input1> [input2] ..."
   exit 1
 fi
 
-# Input and output
-input_file="$1"
-output_file="$2"
+# Loop input files
+for input_file in "$@"; do
 
-# Does input file exist?
-if [ ! -f "$input_file" ]; then
-  echo "Error: The specified input file does not exist."
-  exit 1
-fi
+  # Output file = input file
+  output_file="${input_file%.pdf}-compressed.pdf"
 
-# Array of Ghostscript optionss
-options=("Screen (Low quality)" "Ebook (Medium quality)" "Printer (High quality)" "Prepress (Highest quality)")
+  # Does input file exist?
+  if [ ! -f "$input_file" ]; then
+    echo -e "\e[33mWarning: The specified input file '$input_file' does not exist. Skipping...\e[0m"
+    continue
+  fi
 
-# Display options
-echo "Choose Ghostscript option:"
-select option in "${options[@]}"; do
-  case $REPLY in
-    1) gs_options="-dPDFSETTINGS=/screen"; break;;
-    2) gs_options="-dPDFSETTINGS=/ebook"; break;;
-    3) gs_options="-dPDFSETTINGS=/printer"; break;;
-    4) gs_options="-dPDFSETTINGS=/prepress"; break;;
-    *) echo "Invalid option. Please choose a number between 1 and 4."; continue;;
-  esac
+  # Array of Ghostscript options
+  options=("Screen (Low quality)" "Ebook (Medium quality)" "Printer (High quality)" "Prepress (Highest quality)")
+
+  # Display the list of options in yelloww
+  echo -e "\e[33mChoose Ghostscript option for $input_file to $output_file:\e[0m"
+  select option in "${options[@]}"; do
+    case $REPLY in
+      1) gs_options="-dPDFSETTINGS=/screen"; break;;
+      2) gs_options="-dPDFSETTINGS=/ebook"; break;;
+      3) gs_options="-dPDFSETTINGS=/printer"; break;;
+      4) gs_options="-dPDFSETTINGS=/prepress"; break;;
+      *) echo -e "\e[33mInvalid option. Please choose a number between 1 and 4.\e[0m"; continue;;
+    esac
+  done
+
+  # Run Ghostscript command
+  gs_command="gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite $gs_options -sOutputFile=$output_file $input_file"
+  echo "Running Ghostscript command: $gs_command"
+  $gs_command
+
+  # Check if Ghostscript command was successful
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mConversion successful. Output file: $output_file\e[0m"
+  else
+    echo -e "\e[31mError during conversion. Please check your input and options.\e[0m"
+  fi
 done
-
-# Run Ghostscript command
-gs_command="gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite $gs_options -sOutputFile=$output_file $input_file"
-echo "Running Ghostscript command: $gs_command"
-$gs_command
-
-# Check if Ghostscript command was successful
-if [ $? -eq 0 ]; then
-  echo "Conversion successful. Output file: $output_file"
-else
-  echo "Error during conversion. Please check your input and options."
-fi
