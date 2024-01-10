742423a3445fc2fd09636b2b4f3191de0f5daf46
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-01-10 12:19:30 +0800

    Update tolgas-pdf-cruncher.sh

--- CUSTOM-SCRIPTS/tolgas-pdf-cruncher.sh
+++ CUSTOM-SCRIPTS/tolgas-pdf-cruncher.sh
@@ -2,25 +2,29 @@
 # Tolga Erok
 # pdf size reducer
 
-# Remote:
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/tolgaerok/main/CUSTOM-SCRIPTS/tolgas-pdf-cruncher.sh)"
-
 # Current PWD of script
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Prompt user for PDF file
-read -p "Enter the input PDF file name: " input_file
+# Check arguments is providedd
+if [ "$#" -ne 2 ]; then
+  echo "Usage: pdfcompress <input_file> <output_file>"
+  exit 1
+fi
 
-# Error check if the input file exists
+# Input and output
+input_file="$1"
+output_file="$2"
+
+# Does input file exist?
 if [ ! -f "$input_file" ]; then
   echo "Error: The specified input file does not exist."
   exit 1
 fi
 
-# Array of Ghostscript options
+# Array of Ghostscript optionss
 options=("Screen (Low quality)" "Ebook (Medium quality)" "Printer (High quality)" "Prepress (Highest quality)")
 
-# Display the array list of options
+# Display options
 echo "Choose Ghostscript option:"
 select option in "${options[@]}"; do
   case $REPLY in
@@ -31,9 +35,6 @@ select option in "${options[@]}"; do
     *) echo "Invalid option. Please choose a number between 1 and 4."; continue;;
   esac
 done
-
-# Prompt user for output file location
-read -p "Enter the output file name (include the extension, e.g., output.pdf): " output_file
 
 # Run Ghostscript command
 gs_command="gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite $gs_options -sOutputFile=$output_file $input_file"
