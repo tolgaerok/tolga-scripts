272c68efbe71c349bf34654b39f7b504cf96c41f
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-02-24 22:22:51 +0800

    Create extra-fonts.sh

---
+++ CUSTOM-SCRIPTS/CUSTOM-FONTS/extra-fonts.sh
@@ -0,0 +1,11 @@
+# Tolga Erok
+# 24/2/24
+
+
+sudo dnf install ibm-plex-mono-fonts ibm-plex-sans-fonts ibm-plex-serif-fonts \
+    redhat-display-fonts redhat-text-fonts \
+    lato-fonts \
+    jetbrains-mono-fonts \
+    fira-code-fonts mozilla-fira-mono-fonts mozilla-fira-sans-fonts mozilla-zilla-slab-fonts \
+    adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts \
+    intel-clear-sans-fonts intel-one-mono-fonts
