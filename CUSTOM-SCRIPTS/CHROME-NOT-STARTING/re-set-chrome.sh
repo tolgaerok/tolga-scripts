d8e3809ee2f320ce15e2ff97677030c190c262c1
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-02-24 20:56:05 +0800

    Create re-set-chrome.sh

---
+++ CUSTOM-SCRIPTS/CHROME-NOT-STARTING/re-set-chrome.sh
@@ -0,0 +1,7 @@
+# Tolga Erok
+# 24-2-24
+# if Chrome is misbehaving due to some corrupted Singleton files, and you want to reset them, use this script
+
+#!/bin/bash
+rm -rf ~/.config/google-chrome/Singleton*
+
