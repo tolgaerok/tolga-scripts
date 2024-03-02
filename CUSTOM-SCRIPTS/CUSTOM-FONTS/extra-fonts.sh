8192c76b45ac41f1e6576902b67dc97364065c49
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-03-02 21:18:02 +0800

    Update extra-fonts.sh

--- CUSTOM-SCRIPTS/CUSTOM-FONTS/extra-fonts.sh
+++ CUSTOM-SCRIPTS/CUSTOM-FONTS/extra-fonts.sh
@@ -10,3 +10,6 @@ sudo dnf install ibm-plex-mono-fonts ibm-plex-sans-fonts ibm-plex-serif-fonts \
     fira-code-fonts mozilla-fira-mono-fonts mozilla-fira-sans-fonts mozilla-zilla-slab-fonts \
     adobe-source-serif-pro-fonts adobe-source-sans-pro-fonts \
     intel-clear-sans-fonts intel-one-mono-fonts
+
+sudo dnf install curl cabextract xorg-x11-font-utils fontconfig
+sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
