14bc33148c84231492f92e80a04da5f1fbb0a195
Tolga Erok <kingtolga@gmail.com>
2024-12-22 18:39:30 +0800

    (ツ)_/¯ Edit: 22-12-2024 06:39:30 PM

--- ARCH/BIGLINUX/NEW-INSTALLS-DEC-2024/misc-config.sh
+++ ARCH/BIGLINUX/NEW-INSTALLS-DEC-2024/misc-config.sh
@@ -4,13 +4,16 @@
 # 10/6/2024
 # misc
 
-flatpak install flathub io.github.aandrew_me.ytdn
-flatpak run io.github.aandrew_me.ytdn
+    flatpak install flathub io.github.aandrew_me.ytdn
+    flatpak run io.github.aandrew_me.ytdn
 
-sudo systemctl enable --now smb.service nmb.service && sudo systemctl restart smb.service nmb.service
+    sudo systemctl enable --now smb.service nmb.service && sudo systemctl restart smb.service nmb.service
 
 
 # grub
 # GRUB_CMDLINE_LINUX="io_delay=none rootdelay=0 io_delay=none mitigations=off"
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+# Google chrome
+    pamac build google-chrome
+
