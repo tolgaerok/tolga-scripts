3ce3b4975334c4a0dd7d255aca7955822a81f406
Tolga Erok <kingtolga@gmail.com>
2025-01-06 22:58:35 +0800

    (ツ)_/¯ Edit: 06-01-2025 10:58:35 PM

--- NOBARA-41/CAKE/check-interface-cake.sh
+++ NOBARA-41/CAKE/check-interface-cake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# Tolga
+# version 5
 
 # Get the interface name
-# interface=$(ip link show | awk -F: '/wlp|wlo|wlx/ && !/NO-CARRIER/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2; exit}')
-# interface=\$(ip link show | awk -F: '\$0 ~ /wlp|wlo|wlx/ && \$0 !~ /NO-CARRIER/ {gsub(/^[ \\t]+|[ \\t]+$/, \"\", \$2); print \$2; exit}')
 interface=$(ip link show | awk -F: '$0 ~ "^[2-9]:|^[1-9][0-9]: " && $0 ~ "UP" && $0 !~ "LOOPBACK|NO-CARRIER" {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2; getline}')
 
-# Get the qdisc status 
+# Get the qdisc status for the interface
 output=$(sudo tc qdisc show dev "$interface")
 
 # Define color codes
@@ -15,9 +15,11 @@ RESET="\033[0m"
 
 # Check if 'cake' is present in the output
 if echo "$output" | grep -q 'cake'; then
-    echo -e "\\n${GREEN}Interface $interface is working after suspend${RESET}\\n"
+    echo -e "\\n${GREEN}> Interface $interface is working after suspend${RESET}\\n"
 else
-    echo -e "\\n${RED}Interface $interface did NOT work after suspend${RESET}\\n"
+    echo -e "\\n${RED}> Interface $interface did NOT work after suspend${RESET}\\n"
 fi
 
+sudo systemctl status apply-cake-qdisc.service --no-pager
+echo -e "\\n${GREEN}|------------------------------------------------------------------------------------------------------------------| ${RESET}\\n"
 sudo systemctl status apply-cake-qdisc-wake.service --no-pager
