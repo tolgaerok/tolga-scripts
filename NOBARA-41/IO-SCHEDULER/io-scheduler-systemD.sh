f9ecdeb1ee8fd942bf948dfff40e3af1aa57d815
Tolga Erok <kingtolga@gmail.com>
2025-01-06 12:15:03 +0800

    (ツ)_/¯ Edit: 06-01-2025 12:15:03 PM

---
+++ NOBARA-41/IO-SCHEDULER/io-scheduler-systemD.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# tolga erok
+
+# curl -sL https://raw.githubusercontent.com/tolgaerok/solus/main/PERSONAL-SCRIPTS/IO-SCHEDULER/io-scheduler.sh | sudo bash
+
+# Create the systemd service file for setting the I/O scheduler
+echo "[Unit]
+Description=Set I/O Scheduler on boot for SSD / NVME
+
+[Service]
+Type=simple
+ExecStart=/bin/bash -c 'echo none | sudo tee /sys/block/sda/queue/scheduler; printf \"I/O Scheduler set to: \"; cat /sys/block/sda/queue/scheduler'
+
+[Install]
+WantedBy=default.target" | sudo tee /etc/systemd/system/io-scheduler.service
+
+# Reload systemd, enable, and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable io-scheduler.service
+sudo systemctl start io-scheduler.service
+sudo systemctl status io-scheduler.service
