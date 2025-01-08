983dd574a6fd21ebe727fe2831badd6418d957db
Tolga Erok <kingtolga@gmail.com>
2025-01-08 09:57:19 +0800

    (ツ)_/¯ Edit: 08-01-2025 09:57:19 AM

---
+++ NOBARA-41/CAKE/RESTART-CAKE.sh
@@ -0,0 +1,14 @@
+# Restart cake services
+
+```bash
+alias restart-cake="sudo systemctl daemon-reload && \
+sudo systemctl start apply-cake-qdisc.service && \
+sudo systemctl start apply-cake-qdisc-wake.service && \
+sudo systemctl enable apply-cake-qdisc.service && \
+sudo systemctl enable apply-cake-qdisc-wake.service && \
+sudo systemctl status apply-cake-qdisc.service --no-pager && \
+echo \"\" && \
+echo \"|-----------------------------------------------------------------------------------------------------|\" && \
+echo \"\" && \
+sudo systemctl status apply-cake-qdisc-wake.service --no-pager"
+
