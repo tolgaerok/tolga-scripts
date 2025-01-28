eef5a20f9ff0c8bffd5e84c07de253e183bceac1
Tolga Erok <kingtolga@gmail.com>
2025-01-28 21:06:00 +0800

    (ツ)_/¯ Edit: 28-01-2025 09:06:00 PM

---
+++ ARCH/BIGLINUX/NEW-INSTALLS-DEC-2024/CODE-OSS-EXTENSION-FIX/extra-fonts-code.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.local/share/fonts
+ln -s ~/.local/share/fonts/ ~/.fonts
+cd ~/.fonts
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.tar.xz
+tar -xvf Hack.tar.xz
