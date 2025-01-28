d371d62bcdf13aab3515b487e30b9742fe587313
Tolga Erok <kingtolga@gmail.com>
2025-01-28 21:18:56 +0800

    (ツ)_/¯ Edit: 28-01-2025 09:18:56 PM

--- ARCH/BIGLINUX/NEW-INSTALLS-DEC-2024/CODE-OSS-EXTENSION-FIX/extra-fonts-code.sh
+++ ARCH/BIGLINUX/NEW-INSTALLS-DEC-2024/CODE-OSS-EXTENSION-FIX/extra-fonts-code.sh
@@ -6,3 +6,7 @@ ln -s ~/.local/share/fonts/ ~/.fonts
 cd ~/.fonts
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.tar.xz
 tar -xvf Hack.tar.xz
+
+
+# ORIGINAL = 'Droid Sans Mono', 'monospace', monospace
+# ....  'Cascadia Code', 'Droid Sans Mono', 'monospace','Hack Nerd Font',monospace
