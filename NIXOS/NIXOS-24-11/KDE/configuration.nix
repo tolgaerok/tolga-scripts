bdcf4bb69bd1d87961c7c56bf9a9abbb4c562ad3
Tolga Erok <kingtolga@gmail.com>
2025-02-21 13:44:47 +0800

    (ツ)_/¯ Edit: 21-02-2025 01:44:47 PM

--- NIXOS/NIXOS-24-11/KDE/configuration.nix
+++ NIXOS/NIXOS-24-11/KDE/configuration.nix
@@ -65,8 +65,8 @@ in
 
     # Configure extra hosts
     extraHosts = ''
-      127.0.0.1	      localhost
-      127.0.0.1       HP-G1-800
+      127.0.0.1       localhost
+      127.0.0.1       HP-G4-800
       192.168.0.1     router
       192.168.0.2     DIGA            # Smart TV
       192.168.0.5     folio-F39       # HP Folio
@@ -75,9 +75,9 @@ in
       192.168.0.8     min21THIN       # EMMC thinClient
       192.168.0.10    TUNCAY-W11-ENT  # Dads PC
       192.168.0.11    ubuntu-server   # CasaOS
-      192.168.0.32    HP-G4           # Main NixOS rig
       192.168.0.15    KingTolga       # My mobile
       192.168.0.17    QNAP-SERVER     # Main home server
+      192.168.0.32    HP-G4           # Main NixOS rig
 
       # The following lines are desirable for IPv6 capable hosts
       ::1             localhost ip6-localhost ip6-loopback
