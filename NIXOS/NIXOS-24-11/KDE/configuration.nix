8ed8e9b18caf3411d0e11078b520b3101a4e50da
Tolga Erok <kingtolga@gmail.com>
2025-02-21 13:45:46 +0800

    (ツ)_/¯ Edit: 21-02-2025 01:45:46 PM

--- NIXOS/NIXOS-24-11/KDE/configuration.nix
+++ NIXOS/NIXOS-24-11/KDE/configuration.nix
@@ -280,7 +280,7 @@ in
       # HDD
       ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
       # SSD
-      ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
+      ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
       # NVMe SSD
       ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
     '';
