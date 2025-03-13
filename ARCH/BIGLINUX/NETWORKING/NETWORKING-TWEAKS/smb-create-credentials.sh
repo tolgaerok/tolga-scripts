96ea723922dcd102876f02757459223e3731fc10
Tolga Erok <kingtolga@gmail.com>
2025-03-13 21:10:21 +0800

    (ツ)_/¯ Edit: 13-03-2025 09:10:21 PM

--- ARCH/BIGLINUX/NETWORKING/NETWORKING-TWEAKS/smb-create-credentials.sh
+++ ARCH/BIGLINUX/NETWORKING/NETWORKING-TWEAKS/smb-create-credentials.sh
@@ -6,7 +6,6 @@
 CREDENTIALS_FILE="/etc/samba/credentials"
 
 read -p "Enter your Samba username: " smb_username
-
 read -sp "Enter your Samba password: " smb_password
 echo
 
@@ -15,7 +14,5 @@ echo "Creating Samba credentials file at $CREDENTIALS_FILE..."
 # Write to my credentials file
 echo "username=$smb_username" | sudo tee $CREDENTIALS_FILE > /dev/null
 echo "password=$smb_password" | sudo tee -a $CREDENTIALS_FILE > /dev/null
-
 sudo chmod 600 $CREDENTIALS_FILE
-
 echo "Samba credentials file created successfully with restricted permissions."
